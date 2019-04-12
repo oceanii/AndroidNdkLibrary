42942d7ff15f9345bb75d7334183ab9ba0bf3528
漫步 <260864798@qq.com>
2019-04-12 23:22:41 +0800

    AD20190412-优化底层调用静态库Demo

--- AndroidNdkInvokeStaticLibrary/app/src/main/cpp/native-lib.cpp
+++ AndroidNdkInvokeStaticLibrary/app/src/main/cpp/native-lib.cpp
@@ -8,7 +8,13 @@
 
 #include "CCjsonUtils.h"
 
-extern "C"
+//extern "C"
+//__cplusplus是cpp中的自定义宏, 定义了这个宏表示这是一段cpp的代码
+//下面的代码含义是: 如果这一段是cpp代码, 则以标准C形式进行编译链接
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 
 JNIEXPORT jstring JNICALL Java_com_example_xzy_androidndkinvokestaticlibrary_MainActivity_stringFromJNI(
         JNIEnv *env,
@@ -20,3 +26,8 @@ JNIEXPORT jstring JNICALL Java_com_example_xzy_androidndkinvokestaticlibrary_Mai
 
     return env->NewStringUTF(hello.c_str());
 }
+
+
+#ifdef __cplusplus
+}
+#endif
